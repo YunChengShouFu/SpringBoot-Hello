66d46439358abe55f447b4dff806da64a7627a17
张旭东 <Z13641216104@163.com>
2022-12-25 16:55:24 +0800

    Initial commit-删除输出语句

--- src/main/java/com/zxd/springboothello/service/UserService.java
+++ src/main/java/com/zxd/springboothello/service/UserService.java
@@ -27,7 +27,6 @@ public class UserService {
     @RequestMapping("/findScrm")
     public String findScrm(){
         log.info("info: "+scrm.toString());
-        System.out.println("git111");
         return scrm.toString();
     }
 }
